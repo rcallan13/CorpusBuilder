11191dcccfc012ac3471c33427a8904e5b0c6fd0
rcallan13 <ron_allan@yahoo.com>
2019-03-25 07:00:57 -0700

    Initial Commit

---
+++ CorpusBuilder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CorpusBuilder
+//
+//  Created by Ron Allan on 2019-03-25.
+//  Copyright © 2019 RCAL Software Solutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ CorpusBuilderTests/CorpusBuilderTests.swift
@@ -0,0 +1,34 @@
+//
+//  CorpusBuilderTests.swift
+//  CorpusBuilderTests
+//
+//  Created by Ron Allan on 2019-03-25.
+//  Copyright © 2019 RCAL Software Solutions. All rights reserved.
+//
+
+import XCTest
+@testable import CorpusBuilder
+
+class CorpusBuilderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
