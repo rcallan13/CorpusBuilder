3a9263fd3db263f876b06a54997736c0eac21c3d
rcallan13 <ron_allan@yahoo.com>
2019-03-25 06:56:31 -0700

    initial checkin

---
+++ src/com/rcal/corpusbuilder/CorpusException.java
@@ -0,0 +1,14 @@
+package com.rcal.corpusbuilder;
+
+/**
+ * Created by ronallan on 2019-03-25.
+ */
+public class CorpusException extends Exception {
+
+    public CorpusException() {
+    }
+
+    public CorpusException(String message) {
+        super(message);
+    }
+}
